722dc71ca50d1d0340258541abdf13869850408b
Sagar Jounkani <sagarjounkani@Sagars-MacBook-Pro.local>
2021-07-07 16:50:08 +0530

    updated predict

--- src/predict.py
+++ src/predict.py
@@ -12,14 +12,12 @@ from model import EntityModel
 if __name__ == "__main__":
 
     meta_data = joblib.load("meta.bin")
-    enc_pos = meta_data["enc_pos"]
     enc_tag = meta_data["enc_tag"]
 
-    num_pos = len(list(enc_pos.classes_))
     num_tag = len(list(enc_tag.classes_))
 
     sentence = """
-    abhishek is going to india
+    0403 city mansion apartment 53 langford road bangalore karnataka 560025
     """
     tokenized_sentence = config.TOKENIZER.encode(sentence)
 
@@ -28,29 +26,22 @@ if __name__ == "__main__":
     print(tokenized_sentence)
 
     test_dataset = dataset.EntityDataset(
-        texts=[sentence], 
-        pos=[[0] * len(sentence)], 
+        texts=[sentence],
         tags=[[0] * len(sentence)]
     )
 
-    device = torch.device("cuda")
-    model = EntityModel(num_tag=num_tag, num_pos=num_pos)
+    model = EntityModel(num_tag=num_tag)
     model.load_state_dict(torch.load(config.MODEL_PATH))
-    model.to(device)
+    model.to(config.DEVICE)
 
     with torch.no_grad():
         data = test_dataset[0]
         for k, v in data.items():
-            data[k] = v.to(device).unsqueeze(0)
+            data[k] = v.to(config.DEVICE).unsqueeze(0)
         tag, pos, _ = model(**data)
 
         print(
             enc_tag.inverse_transform(
                 tag.argmax(2).cpu().numpy().reshape(-1)
-            )[:len(tokenized_sentence)]
-        )
-        print(
-            enc_pos.inverse_transform(
-                pos.argmax(2).cpu().numpy().reshape(-1)
             )[:len(tokenized_sentence)]
         )
